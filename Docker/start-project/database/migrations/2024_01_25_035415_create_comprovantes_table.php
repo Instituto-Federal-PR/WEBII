d3a2510d6fb226c382d1a4efc7e90d0f9a64ef51
gileduardo <gilgea@gmail.com>
2024-01-25 01:30:43 -0300

    Aula 01 - Migration e Seeder

---
+++ Docker/start-project/database/migrations/2024_01_25_035415_create_comprovantes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comprovantes', function (Blueprint $table) {
+            $table->id();
+            $table->float('horas');
+            $table->string('atividade');
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('aluno_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->foreign('aluno_id')->references('id')->on('alunos');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comprovantes');
+    }
+};
---
+++ Docker/start-project/database/migrations/2024_01_25_042358_create_documentos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->float('horas_in');
+            $table->string('status');      // SOLICITADO / DEFERIDO / INDEFERIDO
+            $table->string('comentario');
+            $table->float('horas_out');
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documentos');
+    }
+};
