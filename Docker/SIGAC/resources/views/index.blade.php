1339a478467c522052e39585ecd895626309441c
gileduardo <gilgea@gmail.com>
2024-05-11 14:26:50 -0300

    Aula 07 - Autenticação e Autorização

--- Docker/SIGAC/resources/views/index.blade.php
+++ Docker/SIGAC/resources/views/index.blade.php
@@ -8,16 +8,6 @@
         <span class="fw-light text-success fs-3 mb-3">
             Sistema de Gerenciamento de Atividades Complementares
         </span>
-        <hr>
-        <table class="table align-middle caption-top table-striped">
-            <thead>
-                <tr>
-                    <th scope="col" class="text-success">PAPEL</th>
-                    <th scope="col" class="text-success">USUÁRIO</th>
-                    <th scope="col" class="text-success">SENHA</th>
-                </tr>
-            </thead>
-        </table>
     </div>
 
 @endsection
